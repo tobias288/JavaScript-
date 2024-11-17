500d86f9d0d85b117c5416b863da3563f49fbe0d
tobias288 <tobiasuche7@gmail.com>
2024-11-17 05:43:29 +0100

    first javaScript commit

--- pract.js
+++ pract.js
@@ -1,64 +1,64 @@
-// function incrementCounter() {
-//     let a = 0;
+ function incrementCounter() {
+     let a = 0;
 
-//     return {
-//         increment: function() {
-//             a ++;
-//             return a;
-//         },
-//         decrement: function() {
-//             a --;
-//             return a;
-//         },
-//         getCounter: function() {
-//             return a;
-//         },
-//     };
-// };
+     return {
+         increment: function() {
+             a ++;
+             return a;
+         },
+         decrement: function() {
+            a --;
+            return a;
+        },
+        getCounter: function() {
+            return a;
+        },
+    };
+};
 
-// let b = incrementCounter();
+let b = incrementCounter();
 
-// let c = document.getElementById('counter');
-// let d = document.getElementById('incrementButton');
-// let e = document.getElementById('decrementButton');
+let c = document.getElementById('counter');
+let d = document.getElementById('incrementButton');
+let e = document.getElementById('decrementButton');
 
-// function g() {
-//     c.textContent = b.getCounter();
-// };
+function g() {
+    c.textContent = b.getCounter();
+};
 
-// incrementButton.addEventListener('click', () => {
-//     b.increment();
-//     g();
-// });
+incrementButton.addEventListener('click', () => {
+    b.increment();
+    g();
+});
 
-// decrementButton.addEventListener('click', () => {
-//     b.decrement();
-//     g();
-// });
+decrementButton.addEventListener('click', () => {
+    b.decrement();
+    g();
+});
 
-// g();
+g();
 
-// // practice
+// practice
 
-// let name = "Sophia"
-// let Age = 22;
-// let Course = "Web development";
-// let School = "New Horizon";
+let name = "Sophia"
+let Age = 22;
+let Course = "Web development";
+let School = "New Horizon";
 
 // // Write your code here
 // //console.log(`Hello ${name}, you are ${Age} years old, studying ${Course} at ${School}`)
 // console.log('Hello ' + name + "," + "you are " + Age + "years old, studying " + Course + " at " + School)
 // // concatenate and console the code
 
-// let age = [20, 25, 30, 45, 50];
+let age = [20, 25, 30, 45, 50];
 
-// let a = age.map(agee => agee.length());
+let a = age.map(agee => agee.length());
 
-// if (a == 25) {
-//     console.log(`Welcome onboard`);
-// } else {
-//     console.log(`You are not welcome here`)
-// }
+if (a == 25) {
+    console.log(`Welcome onboard`);
+} else {
+    console.log(`You are not welcome here`)
+}
 
 //
 
@@ -85,24 +85,24 @@ console.log(coding(5, 5))
 
 // write your code here
 
-// const fruits6 = ['Apple', 'Banana', 'Cherry'];
-// let uppercaseFruits = fruits6.map(fruit => fruit.toUpperCase());
-// console.log(uppercaseFruits);
+const fruits6 = ['Apple', 'Banana', 'Cherry'];
+let uppercaseFruits = fruits6.map(fruit => fruit.toUpperCase());
+console.log(uppercaseFruits);
 
-// for (let i = 0; i < Items.length; i++) { // this loop from 0 - 4
-//     console.log(Items[i])
-//     if(i === 3){
-//         break;
-//     }
-// };
+for (let i = 0; i < Items.length; i++) { // this loop from 0 - 4
+    console.log(Items[i])
+    if(i === 3){
+        break;
+    }
+};
 
-// let h = ['rice', 'beans', 'yam', 'potato', 'spagetti'];
-// let i = 'spagetti';
+let h = ['rice', 'beans', 'yam', 'potato', 'spagetti'];
+let i = 'spagetti';
 
-// switch (false) {
-//     case h.includes(i):
-//         console.log(`${i} is included in the list of array`);
-//         break;
-//     default:
-//         console.log(`${i} is not included in the list of array`);
-// };
+switch (false) {
+    case h.includes(i):
+        console.log(`${i} is included in the list of array`);
+        break;
+    default:
+        console.log(`${i} is not included in the list of array`);
+};
